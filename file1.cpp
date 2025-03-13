bb140ebe2354e5a70e907ca765e5e84f33b22908
ArbyPangestu <arbypangestu340@gmail.com>
2025-03-13 11:35:23 +0700

    first commit

--- file1.cpp
+++ file1.cpp
@@ -1,4 +1,4 @@
-#include <iosttream>
+#include <iostream>
 #include <string>
 
 using namespace std;
@@ -23,17 +23,18 @@ void input(){   //Proseur input
     cout << "Masukkan elemen array" << endl;
     cout << "=====================" << endl;
 
-    for (int i=0;i<n;1++){
+    for (int i=0;i<n; i++){
         cout << "Data ke-" << (i+1) << " = ";
         cin >> arr[i];
     }
 }
 
-void bubbleSortArray(){
+void bubbleSortArray()
+{
     int pass = 1;   // step 1
 
     do {
-        for (int j=0; j<=n-1-pass; j++;){   // step 2
+        for (int j=0; j<=n-1-pass; j++){   // step 2
             if (arr[j] > arr[j+1]) {        // step 3
                 int temp;
                 temp = arr[j];
@@ -43,8 +44,7 @@ void bubbleSortArray(){
 
         }
         pass = pass + 1; // step 4
-    }
-
+    }while(pass <= n-1); // step 5
 }
 
 void display (){
